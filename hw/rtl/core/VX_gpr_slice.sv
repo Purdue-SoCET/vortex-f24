25cffb6a653a81b553dd47e7c3354d267bd4d4b4
Muhammad Zohaib Ali <142944109+ali-191@users.noreply.github.com>
2025-02-21 18:50:19 -0500

    Updated VX_gpr_slice.sv

--- hw/rtl/core/VX_gpr_slice.sv
+++ hw/rtl/core/VX_gpr_slice.sv
@@ -15,7 +15,8 @@
 
 module VX_gpr_slice import VX_gpu_pkg::*; #(
     parameter CORE_ID = 0,
-    parameter CACHE_ENABLE = 0
+    parameter CACHE_ENABLE = 0,
+    parameter NUM_THREADS = 0
 ) (
     input wire              clk,
     input wire              reset,
